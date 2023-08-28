f83f8003a617d93216797ca64d5348ecac9e9aa0
AntoCreed777 <abenavides@desem.cl>
2023-08-28 13:31:03 -0400

    Problema_11 con validacion de numeros aaaaa

--- Listado_Evaluado_1/Problema_11.c
+++ Listado_Evaluado_1/Problema_11.c
@@ -1,28 +1,66 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+
+void ingreso(char mensaje[],char **cadena,int *longitud){    //Funcion que valida que solo ingresen numeros
+    int tamal=1;
+    char caracter;
+    while (tamal==1){ 
+        *longitud=0;
+        int capacidad=0;
+        printf("%s",mensaje);
+        while((caracter=getchar())!='\n'){
+            if (*longitud>=capacidad){
+                capacidad+=2;
+                *cadena=(char*)realloc(*cadena,sizeof(char)*capacidad);
+            }
+            (*cadena)[*longitud]=caracter;
+            (*longitud)++;
+        }
+        tamal=0;
+        for(int i=0;i<*longitud;i++){
+            if((*cadena)[i]<48 || (*cadena)[i]>57){
+                tamal=1;
+                printf("Solo se aceptan numeros\n\n");
+                break;
+            }
+        }
+    }
+}
+
 int main(){
     //Ingreso
-    int n;
-    printf("Ingrese la cantidad de notas: ");
-    scanf("%d",&n);
+    char *cadena=NULL;
+    int longitud;
+    ingreso("Ingrese la cantidad de notas: ",&cadena,&longitud);
+    int n=atoi(cadena);
+
     while(n<1 || n>10){
-        printf("Debe estar entre 1 y 10\nIngrese la cantidad de notas: ");
-        scanf("%d",&n);
+        free(cadena);
+        cadena=NULL;
+        ingreso("Debe estar entre 1 y 10\nIngrese la cantidad de notas: ",&cadena,&longitud);
+        n=atoi(cadena);
     }
 
     //Declaracion de variables
     int *notas;
     notas=(int*)malloc(sizeof(int)*n);
-    int aux=0;
+    char *auxchar=NULL;
+    int longitudauxchar;
 
     //Ingreso de las notas
     for(int i=0;i<n;i++){
-        printf("Ingrese la nota numero %d: ",i+1);
-        scanf("%d",&aux);
+        char mensaje[30];
+        sprintf(mensaje, "Ingrese la nota numero %d: ", i + 1);
+        ingreso(mensaje,&auxchar,&longitudauxchar);
+        int aux=atoi(auxchar);
+
         while(aux<3 || aux>5){
-            printf("La nota debe estar entre 3 y 5\nIngrese la nota numero %d: ",i+1);
-            scanf("%d",&aux);
+            printf("La nota debe estar entre 3 y 5\n\n");
+            free(auxchar);
+            auxchar=NULL;
+            ingreso(mensaje,&auxchar,&longitudauxchar);
+            aux=atoi(auxchar);
         }
         notas[i]=aux;
     }
@@ -44,16 +82,16 @@ int main(){
     //Si obtiene un resultado académico alto “Alto”
     //Si obtiene un resultado personal “Nombrado”.
     if(media<4){
-        printf("Ninguno");
+        printf("None");
     }
     else if(media==5){
-        printf("Nombrado");
+        printf("Named");
     }
     else if(media>=4.5){
-        printf("Alto");
+        printf("High");
     }
     else{
-        printf("Comun");
+        printf("Common");
     }
 
     return 0;
