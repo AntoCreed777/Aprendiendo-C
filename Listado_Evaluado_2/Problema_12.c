a3bfbca961844c12c8a6431844a4849285204648
AntoCreed777 <abenavides@desem.cl>
2023-10-14 14:20:13 -0300

    Aceptado por TIMUS, !!FALTA COMENTAR BIEN!!!

--- Listado_Evaluado_2/Problema_12.c
+++ Listado_Evaluado_2/Problema_12.c
@@ -4,7 +4,7 @@
 
 int main(){
     //Declaracion del String que recibira la palabra entrante
-    char entrada[100];
+    char entrada[101];
     scanf("%s",entrada);
     int len=strlen(entrada);
 
@@ -15,35 +15,29 @@ int main(){
     for(int i=0;i<len;i++){
         numeros[i]=entrada[i]-'a';
     }
-
-
-    int anterior=numeros[0];    //Variable que almacenara el valor anterior al ciclo actual para comparar
-    int actual;                 //Variable que almacenara el valor actual al ciclo para comparar
     
     for(int i=1;i<len;i++){     //for que desace el modulo por 26 que se aplico a la hora de cifrarlo
-        actual=numeros[i];
-        while(actual<anterior){
+        while(numeros[i]<numeros[i-1]){
             numeros[i]+=26;
-            actual=numeros[i];
         }
-        anterior=actual;
     }
 
     for(int i=len-1;i>0;i--){   //for que desace la suma de los numeros
-        numeros[i]=numeros[i]-numeros[i-1];
-    }
-    numeros[0]-=5;  //Se le resta 5 para obtener el numero original
-
-    //Obtengo las letras que representan cada numero
-    for(int i=0;i<len;i++){
-        entrada[i]=numeros[i]+'a';
+        numeros[i]-=numeros[i-1];
     }
 
-    for(int i=0;i<len;i++){     //Impresion de las letras finales
-        printf("%c",entrada[i]);
+    if(numeros[0]>= 5){
+        numeros[0]-=5;  //Se le resta 5 para obtener el numero original
+    }
+    else{
+        numeros[0] += 21;
     }
     
-    free(numeros);
+    for(int i=0;i<len;i++){     //Impresion de las letras resultantes
+        printf("%c",(char)(numeros[i]+'a'));
+    }
     
+    free(numeros);  //Liberacion de memoria
+
     return 0;
 }
