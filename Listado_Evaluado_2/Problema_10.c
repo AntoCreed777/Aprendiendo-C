3f56471dc45845320e0806d2e4b16156dc7a71c2
AntoCreed777 <abenavides@desem.cl>
2023-10-16 19:45:52 -0300

    //Comentado pero falta revisar con Valgrind

--- Listado_Evaluado_2/Problema_10.c
+++ Listado_Evaluado_2/Problema_10.c
@@ -7,19 +7,21 @@ int main(){
 
     for(int j=0;j<12;j++){
         for(int k=0;k<16;k++){
-            precipitaciones[j][k]=0;
+            precipitaciones[j][k]=0;    //Se asigna 0 a todas los valores del array
         }
     }
 
+    //Declaracion del array meses y regiones para facilitar la impresion luego
     char meses[12][11]={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
     
     char regiones[16][30]={"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI"};
     
-    srand (time(NULL));
+    srand (time(NULL)); //Se inicializa la semilla del random
 
-    for(int i=0;i<15;i++){
-        for(int j=0;j<12;j++){
-            for(int k=0;k<16;k++){
+    for(int i=0;i<15;i++){          //Se recorren los años
+        for(int j=0;j<12;j++){      //Se recorren los meses
+            for(int k=0;k<16;k++){  //Se recorren las regiones
+                //Hay distintos random por Estacion Climatica para que sea mas realista
                 if(j>=0 && j<=2){   //Verano
                     precipitaciones[j][k]+=rand()%600;
                 }
@@ -37,6 +39,7 @@ int main(){
         }
     }
 
+    //Se imprime el eje de la regiones
     for(int k=0;k<16;k++){
         if(k==0){
             printf("\t\t");
@@ -45,13 +48,13 @@ int main(){
     }
     printf("\n");
 
-    for(int j=0;j<12;j++){
-        for(int k=0;k<16;k++){
-            precipitaciones[j][k]/=15;
-            if(k==0){
+    for(int j=0;j<12;j++){                          //Se recorren los meses
+        for(int k=0;k<16;k++){                      //Se recorren las regiones
+            precipitaciones[j][k]/=15;              //Se calcula el promedio de la suma de las presipitaciones en el mes "i" y region "k"
+            if(k==0){                               //Se imprime el eje de los meses en la primera columna
                 printf("%11s\t",meses[j]);
             }
-            printf("%4d\t",precipitaciones[j][k]);
+            printf("%4d\t",precipitaciones[j][k]);  //Se imprime las presipitaciones promedio en el mes "i" y region "k"
         }
          printf("\n");
     }
