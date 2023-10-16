000eedbe0234679667d06f9cf7fbe0838ba1e43a
AntoCreed777 <abenavides@desem.cl>
2023-10-16 19:20:50 -0300

    Comentado y listo para entregar

--- Listado_Evaluado_2/Problema_5.c
+++ Listado_Evaluado_2/Problema_5.c
@@ -1,20 +1,25 @@
+/*Escriba un programa que pida la posición de un alfil en un tablero de ajedrez,
+para que luego reporte todos los movimientos válidos del alfil. Para reportar las
+posiciones, imprima por pantalla el tablero de ajedrez, marcando con una A la posición
+ingresada por teclado y con x’s las posibles posiciones válidas del alfil*/
+
 #include <stdio.h>
 
 void impresion_matriz(char matriz[8][8]){   //Impresion del tablero
-    printf("  ----------------\n");     //Marco Superior
-    for(int i=0;i<8;i++){
-        for(int j=0;j<11;j++){
+    printf("  ----------------\n");         //Marco Superior
+    for(int i=0;i<8;i++){                   //Se recorren las filas de la impresion
+        for(int j=0;j<11;j++){              //Se recorren las columnas de la impresion
             if(j==0){
-                printf("%d",8-i);   //Coordenadas del eje y
+                printf("%d",8-i);           //Coordenadas del eje y
             }
             else if(j==1 || j==10){
-                printf("|");    //Marcos laterales
+                printf("|");                //Marcos laterales
             }
             else{
                 printf("%c ",matriz[i][j-2]);   //Elementos del tablero
             }
         }
-        printf("\n");
+        printf("\n");               //Se imprime un salto de linea
     }
     printf("  ----------------\n"); //Marco Inferior
     printf("  a b c d e f g h");    //Coordenadas del eje x
@@ -42,7 +47,7 @@ int main(){
     while(scanf(" %c%d",&x1,&y)!=2 || y<=0 || y>8 || x1<'a' || x1>'h'){   //Ingreso de la coordenada en notacion de ajedrez
         printf("\nError de entrada\nValor fuea de rango[a,h][1,8]:  \n\nIngrese la coordenada (Ej: d4): ");
         int c;
-        while ((c = getchar()) != '\n' && c != EOF) {}  // Limpio la  entrada para eliminar caracteres no válidos
+        while ((c = getchar()) != '\n' && c != EOF) {}  // Limpio la entrada para eliminar caracteres no válidos
     }
 
     int x=x1-'a';   //Transformacion de coordenada de letra a numerico
