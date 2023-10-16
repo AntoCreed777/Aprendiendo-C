63373215e9a70f8be587be1beeb9367b7d6a10f0
AntoCreed777 <abenavides@desem.cl>
2023-10-16 17:53:48 -0300

    Bien comentado, listo para entregar

--- Listado_Evaluado_2/Problema_1.c
+++ Listado_Evaluado_2/Problema_1.c
@@ -1,61 +1,65 @@
+/*Escriba un programa que pida dos números enteros n y m, para que luego pida ingresar suficientes valores
+enteros hasta llenar un arreglo A de largo n. Su programa deberá generar dos arreglos B y C, donde B contendrá
+todos los valores del arreglo A que sean menores a m, mientras que C contendrá todos los valores del arreglo A
+que sean mayores o iguales a m.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
-    //Ingreso y validacion del valor N
-    int N;
-    printf("Ingrese la cantidad de numeros que ingresara:   "); 
-    while(scanf("%d",&N)!=1 || N<=0){
-        printf("Error de entrada\n\nIngrese la cantidad de numeros que ingresara:   ");
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF) {}  // Limpio la  entrada para eliminar caracteres no válidos
+    
+    int N;                                                                  //Declaracion de N que representa el largo de un arreglo y la cantidad de valores que ingresan
+    printf("Ingrese la cantidad de numeros que ingresara:   ");             //Mensaje guia para el usuario
+    while(scanf("%d",&N)!=1 || N<=0){                                       //Ingreso y validacion del valor N
+        printf("Error de entrada\n\nIngrese la cantidad de numeros que ingresara:   "); //Mensaje guia para el usuario
+        int c;                                                              //Declaracion de caracter auxiliar
+        while ((c = getchar()) != '\n' && c != EOF) {}                      // Limpio la entrada para eliminar caracteres no válidos
     }
 
-    //Ingreso y validacion del valor M
-    int M;
-    printf("Ingrese el numero delimitador:   ");
-    while(scanf("%d",&M)!=1 || M<=0){
-        printf("Error de entrada\n\nIngrese el numero delimitador:   ");
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF) {}  // Limpio la  entrada para eliminar caracteres no válidos
+    
+    int M;                                                                  //Declaracion de M que representa el valor por el cual se ordenaran los demas numeros
+    printf("Ingrese el numero delimitador:   ");                            //Mensaje guia para el usuario
+    while(scanf("%d",&M)!=1 || M<=0){                                       //Ingreso y validacion del valor M
+        printf("Error de entrada\n\nIngrese el numero delimitador:   ");    //Mensaje guia para el usuario
+        int c;                                                              //Declaracion de caracter auxiliar
+        while ((c = getchar()) != '\n' && c != EOF) {}                      // Limpio la entrada para eliminar caracteres no válidos
     }
 
-    //Creacion y llenado de la matriz A que guardara los numeros que ingrese el usuario
-
-    int *A=(int*)malloc(sizeof(int)*N);
-    int aux;
+    int *A=(int*)malloc(sizeof(int)*N);                                     //Creacion y llenado de la matriz A que guardara los numeros que ingrese el usuario
+    int aux;                                                                //Declaracion de una variable auxiliar que guardara de forma momentania los numeros ingresados
     for(int i=0;i<N;i++){
-        printf("Ingrese el numero de la posicion %d:   ",i+1);
-        while(scanf("%d",&aux)!=1 || aux<=0){
-            printf("Error de entrada\n\nIngrese el numero de la posicion %d:   ",i+1);
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF) {}  // Limpio la  entrada para eliminar caracteres no válidos
+        printf("Ingrese el numero de la posicion %d:   ",i+1);              //Mensaje guia para el usuario
+        while(scanf("%d",&aux)!=1 || aux<=0){                               //Ingreso y validacion de los numeros a ordenar
+            printf("Error de entrada\n\nIngrese el numero de la posicion %d:   ",i+1);  //Mensaje guia para el usuario
+            int c;                                                          //Declaracion de caracter auxiliar
+            while ((c = getchar()) != '\n' && c != EOF) {}                  // Limpio la entrada para eliminar caracteres no válidos
         }
-        A[i]=aux;
+        A[i]=aux;                                                           //Asignacion del numero en el arreglo oficial
     }
     
-    //Creacion de las matrices B yC
+    //Creacion de las matrices B y C
 
     int *B=(int*)malloc(sizeof(int)*N);
     int *C=(int*)malloc(sizeof(int)*N);
 
-    //Comparo los elementos de A con el numero M para repartirlos entre B o C
     
+    //Declaracion de variables auxiliares que guardaran la longitud de sus respectivos arreglos
     int cantidadB=0;
     int cantidadC=0;
+
+    //Comparo los elementos de A con el numero M para repartirlos entre B o C
     for(int i=0;i<N;i++){
-        if(A[i]<M){
+        if(A[i]<M){                     //Si el elemento de A es menos a M entonces se guarda en B
             B[cantidadB]=A[i];
-            cantidadB++;
+            cantidadB++;                //Se aumenta el contador de elementos de B
         }
-        else{
+        else{                           //Si es Mayor o Menor a M entonces se guarda en C
             C[cantidadC]=A[i];
-            cantidadC++;
+            cantidadC++;                //Se aumenta el contador de elementos de C
         }
     }
 
     //Reduccion de los arreglos B y C segun la cantidad de elementos que poseen
-
     B=(int*)realloc(B,sizeof(int)*cantidadB);
     C=(int*)realloc(C,sizeof(int)*cantidadC);
 
@@ -75,6 +79,7 @@ int main(){
         printf("%d\t",C[i]);
     }
 
+    //Liberacion de la memoria asignada con malloc y realloc
     free(A);
     free(B);
     free(C);
