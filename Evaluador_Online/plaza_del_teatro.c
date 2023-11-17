c7807e80862313b580977edc811086a4ecde8294
AntoCreed777 <abenavides@desem.cl>
2023-11-17 12:17:08 -0300

    Problema C alfin terminado

--- Evaluador_Online/plaza_del_teatro.c
+++ Evaluador_Online/plaza_del_teatro.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int n,m,a;
-    scanf("%d %d %d",&n,&m,&a);
+    long long int n,m,a;
+    scanf("%lld %lld %lld",&n,&m,&a);
     if(n<1 || m<1 || a<1 || n>1000000000 || m>1000000000 || a>1000000000){return 0;}
-    int contador1=0,contador2=0,aux1=0,aux2=0;
-    while(contador1<n){
-        contador1+=a;
-        aux1++;
-    }
-    while(contador2<m){
-        contador2+=a;
-        aux2++;
-    }
-    printf("%d",aux1*aux2);
+
+    long long int aux1 = (n/a);
+    if(n%a != 0)aux1++;
+
+    long long int aux2 = (m/a);
+    if(m%a != 0)aux2++;
+
+    printf("%lld",aux1*aux2);
     return 0;
 }
